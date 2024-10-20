5afa63b0f274e9ae5a0a0e84864f3ceb97cbc00c
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2024-10-20 16:17:09 +0300

    Menu component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BooksComponent } from './components/books/books.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ReservationsComponent } from './components/reservations/reservations.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component:HomeComponent
+    },
+    {
+        path:'books', component:BooksComponent
+    },
+    {
+        path:'customers', component:CustomersComponent
+    },
+    {
+        path:'reservations', component:ReservationsComponent
+    },
+
+];
