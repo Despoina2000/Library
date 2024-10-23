dfeef4863f20031ceab4a021cd0631a1cd3173d2
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2024-10-23 15:15:14 +0300

    Card for books section

--- src/app/components/menu/books/books.component.ts
+++ src/app/components/menu/books/books.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import {  DatePipe} from '@angular/common';
 import { Book } from '../../../interfaces/books-api';
 import { BookService } from '../../../services/book/book.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-books',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule,DatePipe],
   templateUrl: './books.component.html',
   styleUrl: './books.component.css',
 })
 export class BooksComponent {
   books: Array<Book>|null= [];
   constructor(private bookServise:BookService){
-    this.bookServise.getAllBooks().subscribe((data)=>{
-       this.books=data;
-    });
+    try{
+      this.bookServise.getAllBooks().subscribe((data)=>{
+        this.books=data;
+     });
+    }
+    catch( HttpErrorResponse){
+      this.books=null;
+    }
+    
   }
 }
