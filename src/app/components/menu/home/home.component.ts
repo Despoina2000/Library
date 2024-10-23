f24f80b72a49bfe0660a28804084ab0ab01e66b5
Despoina Papadopoulou <66719546+Despoina2000@users.noreply.github.com>
2024-10-23 16:34:17 +0300

    home and icon

--- src/app/components/menu/home/home.component.ts
+++ src/app/components/menu/home/home.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import {  DatePipe} from '@angular/common';
+import { Book } from '../../../interfaces/books-api';
+import { BookService } from '../../../services/book/book.service';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButtonModule,DatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  books: Array<Book>|null= [];
+  constructor(private bookServise:BookService){
+    try{
+      this.bookServise.getAllBooks().subscribe((data)=>{
+        data?.forEach(element => {
+          if(element.available==true){
+            this.books?.push(element);
+          }
+          
+        });
+     });
+    }
+    catch( HttpErrorResponse){
+      this.books=null;
+    }
+  }
 
 }
